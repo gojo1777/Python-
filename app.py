fca4f104090360610f37a810d5c45d965e27e91d
gojo1777 <gojomovie0@gmail.com>
2026-05-12 11:35:59 +0530

    Update app.py

--- app.py
+++ app.py
@@ -6,4 +6,6 @@ app = Flask(__name__)
 def home():
     return render_template("index.html")
 
-app.run(debug=True)
+# ❌ DO NOT RUN SERVER IN GITHUB ACTIONS
+if __name__ == "__main__":
+    app.run()
